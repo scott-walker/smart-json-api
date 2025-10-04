080a753b269154c0ca5de61b7dce4f5e0868bd81
Scott Walker <scott@swlt.ru>
2025-10-04 23:12:06 +0300

    Добавлен компонент Header с базовой разметкой и стилями, а также экспортирован из нового файла index.ts.

---
+++ front/src/widgets/header/ui/index.tsx
@@ -0,0 +1,12 @@
+"use client"
+
+/**
+ * Header component
+ */
+export const Header = () => {
+  return (
+    <div className="px-12 py-6 bg-foreground/10 text-foreground">
+      <h1 className="text-4xl font-bold">SmartJSON API</h1>
+    </div>
+  )
+}
