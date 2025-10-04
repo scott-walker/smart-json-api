724bf9486ac53e055af01a32868d800a40a51ecf
Scott Walker <scott@swlt.ru>
2025-10-04 22:18:39 +0300

    Удалены настройки HMR из конфигурации Next.js, упрощая файл next.config.ts.

--- front/next.config.ts
+++ front/next.config.ts
@@ -5,17 +5,6 @@ const nextConfig: NextConfig = {
   devIndicators: {
     position: "bottom-right",
   },
-
-  // Настройки для HMR через прокси
-  webpack: (config, { dev, isServer }) => {
-    if (dev && !isServer) {
-      config.watchOptions = {
-        poll: 1000,
-        aggregateTimeout: 300,
-      };
-    }
-    return config;
-  },
 };
 
 export default nextConfig;
