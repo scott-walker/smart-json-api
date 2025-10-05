06168bb1dc226be3e17130287fbb86e78ae1a5a4
Scott Walker <scott@swlt.ru>
2025-10-05 23:37:12 +0300

    Изменен заголовок в компоненте Header: уменьшен размер шрифта и добавлено выделение слова "JSON" для улучшения визуального восприятия.

--- front/src/widgets/header/ui/index.tsx
+++ front/src/widgets/header/ui/index.tsx
@@ -6,9 +6,8 @@
 export const Header = () => {
   return (
     <div className="px-12 py-6">
-      <h1 className="text-6xl font-display">
-        SmartJSON API
-        {/* <span className="bg-gradient text-background rounded-xl px-4 py-1 font-bold">SmartJSON API</span> */}
+      <h1 className="text-5xl">
+        Smart<strong>JSON</strong> API
       </h1>
     </div>
   )
